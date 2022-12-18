72eb7b25352f0b1adbcc1c5f82961ca7363f6dd4
felipe-almeida-gomes <falmeidagomes13@gmail.com>
2022-12-18 17:54:03 -0300

    Correção na formatação

--- js/script.js
+++ js/script.js
@@ -1,18 +1,49 @@
-const myName = document.querySelector('#myName')
-const arrayMyName = ['f', 'e', 'l', 'i', 'p', 'e', ' ', 'd', 'e', ' ', 'a', 'l', 'm', 'e', 'i', 'd', 'a', ' ', 'g', 'o', 'm', 'e', 's']
+const button = document.querySelector('.menuButton');
+const navegarionMenu = document.querySelector('.navegarionMenu');
+const myName = document.querySelector('#myName');
 
+const arrayMyName = [
+	'f',
+	'e',
+	'l',
+	'i',
+	'p',
+	'e',
+	' ',
+	'd',
+	'e',
+	' ',
+	'a',
+	'l',
+	'm',
+	'e',
+	'i',
+	'd',
+	'a',
+	' ',
+	'g',
+	'o',
+	'm',
+	'e',
+	's',
+];
 
-window.addEventListener('scroll', function(){
-    const header = document.querySelector('header')
-    header.classList.toggle('sticky', window.scrollY > 0)
-})
+window.addEventListener('scroll', function () {
+	const header = document.querySelector('header');
+	header.classList.toggle('sticky', window.scrollY > 0);
+});
 
 const typing = () => {
-    arrayMyName.forEach((element, i) => {
-        setTimeout(() => {
-            myName.innerHTML += element
-        }, i * 100);
-    });
-}
+	arrayMyName.forEach((element, i) => {
+		setTimeout(() => {
+			myName.innerHTML += element;
+		}, i * 100);
+	});
+};
 
-typing()
+const toggleMenu = () => {
+	button.classList.toggle('active');
+	navegarionMenu.classList.toggle('display');
+};
+
+typing();
