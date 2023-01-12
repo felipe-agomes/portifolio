69ea45c1076d497006f5b49d2942713f093860d3
felipe-almeida-gomes <falmeidagomes13@gmail.com>
2023-01-11 23:22:47 -0300

    Update feat das cartas de projeto

--- js/script.js
+++ js/script.js
@@ -7,14 +7,20 @@ const boxProjectCard = document.querySelector('.boxProjectCard')
 const classListBoxProject = boxProjectCard.classList
 const myName = 'felipe de almeida gomes'
 const arrayMyName = myName.split('')
-const objProjectVideo = [
+const arrProjectVideo = [
 	{
-		title: 'work in progress 1',
+		title: 'work in progress'.toUpperCase(),
 		aboutTitle: 'work in progress title 1',
 		aboutContent:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat libero dolore recusandae. In, id, magnam fugiat expedita libero molestias, architecto ipsam saepe magni provident nesciunt voluptate commodi consequuntur nisi corrupti?',
-		href: '../img/teste.mp4',
+			'o que é o que é clara e salgada cabe em um olho e pesa uma tonelada tem sabor de mar pode ser discreta inquilina da dor morada predileta na calada ela vem refém da vingança irmã do desespero rival da esperança pode ser causada por vermes e mundanas e o espinho da flor cruel que você ama amante do drama vem para minha cama por querer sem me perguntar me fez sofrer e eu que me julguei forte eu que me senti serei um fraco quando outras delas virem se o barato é louco e o processo é lento no momento deixa eu caminhar contra o vento do que adianta eu ser durão e o coração ser vulnerável o vento não ele é suave mas é frio e implacável é quente borrou a letra triste do poeta correu no rosto pardo do profeta verme sai dareta a lagrima de um homem vai cair esse é seu bo para eternidade se diz que homem não chora está bom falow então vai para grupo irmão Jesus chorou',
+		src: '../img/teste.mp4',
 	},
+	{
+		title: 'Jogo da Memória'.toUpperCase(),
+		aboutTitle: 'Jogo da Memória',
+		aboutContent: 'Um jogo da Memória criado com apenas JavaScript puro, HTML e CSS. Esse foi uma ideia de um projeto para praticar a lógica de programação e exeritar um pouco',
+		src: '../img/jogoDaMemoria.mp4'
+	}
 ]
 
 window.addEventListener('scroll', function () {
@@ -69,9 +75,20 @@ const toggleBoxProjects = () => {
 
 const displayBoxProjects = ({ target }) => {
 	document.querySelector('.btn').addEventListener('click', toggleBoxProjects)
+	const videoElement = document.querySelector('.boxVideo').lastElementChild
 	toggleBoxProjects()
-
-	target.parentNode.innerText
+	for (let i = 0; i < arrProjectVideo.length; i++) {
+		if (arrProjectVideo[i].title == target.parentNode.innerText) {
+			const aboutTitle = document.querySelector('.boxAbout > h2')
+			const aboutContent = document.querySelector('.boxAbout > p')
+			const boxHeader = document.querySelector('.boxHeader > h2')
+			const video = arrProjectVideo[i].src
+			videoElement.setAttribute('src', video)
+			boxHeader.innerHTML = arrProjectVideo[i].title
+			aboutTitle.innerHTML = arrProjectVideo[i].aboutTitle
+			aboutContent.innerHTML = arrProjectVideo[i].aboutContent
+		}
+	}
 }
 
 arrayProjects.forEach((project) => {
